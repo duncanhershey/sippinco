7bbef5d6d7dc562fa6fdd9e1bacdb225751b7adb
Alwerdani <alwerdani@gmail.com>
2015-12-02 12:05:31 +0200

    back to sqlite database on dev mode

--- coffeedapp/settings/dev.py
+++ coffeedapp/settings/dev.py
@@ -4,15 +4,24 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'coffeeapp',
-        'USER': 'coffeeuser',
-        'PASSWORD': 'No-Password',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+if ON_HEROKU == '1':
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 MIDDLEWARE_CLASSES += (
